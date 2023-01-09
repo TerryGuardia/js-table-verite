9f768fa97ea58f4ca60edbce012334fc79d1b32a
Terry Guardia <74117979+TerryGuardia@users.noreply.github.com>
2023-01-09 18:15:34 +0100

    Add files via upload

---
+++ table_verite/main.js
@@ -0,0 +1,47 @@
+let a;
+
+//non(a et b)
+
+if (!(a && b)) {
+
+}
+
+//non(a ou b)
+if (!(a || b)) {
+
+}
+
+//non(a) et b
+if (!a && b) {
+
+}
+
+//non(a) ou b
+if (!a || b) {
+
+}
+
+//non(b)
+if (!b) {
+
+}
+
+//non(b) et non (a)
+if (!b && !a) {
+
+}
+
+//non(b) ou non (a)
+if (!b || !a) {
+
+}
+
+//a et non(b)
+if (a && !b) {
+
+}
+
+//a ou non(b)
+if (a || !b) {
+
+}
